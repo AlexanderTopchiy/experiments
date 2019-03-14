8c0dea5d9d212027024c80ba4624558f7fa94331
Alexander Topchiy <topchiyau@gmail.com>
2019-03-14 21:17:36 +0300

    Implemented PlayersDAO.

---
+++ Recyclers/app/src/main/java/com/wyverx/recyclers/data/db/PlayersDAO.java
@@ -0,0 +1,15 @@
+package com.wyverx.recyclers.data.db;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+
+import com.wyverx.recyclers.domain.players.models.Player;
+
+import java.util.List;
+
+@Dao
+public interface PlayersDAO {
+
+    @Query("SELECT * FROM players ORDER BY _id ASC")
+    List<Player> getPlayersInfo();
+}
