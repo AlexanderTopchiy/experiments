b6751fb034bcf692aeb4b348a9ccc393a3b604d4
Alexander Topchiy <topchiyau@gmail.com>
2019-04-13 15:20:37 +0300

    Some refactoring of ThreadExecutor.

--- Recyclers/app/src/main/java/com/wyverx/recyclers/domain/threading/ThreadExecutor.java
+++ Recyclers/app/src/main/java/com/wyverx/recyclers/domain/threading/ThreadExecutor.java
@@ -14,16 +14,15 @@ public class ThreadExecutor implements Executor {
     private static final int                     MAX_POOL_SIZE   = 5;
     private static final int                     KEEP_ALIVE_TIME = 120;
     private static final TimeUnit                TIME_UNIT       = TimeUnit.SECONDS;
-    private static final BlockingQueue<Runnable> WORK_QUEUE      = new LinkedBlockingQueue<Runnable>();
+    private static final BlockingQueue<Runnable> WORK_QUEUE      = new LinkedBlockingQueue<>();
     private ThreadPoolExecutor                   mThreadPoolExecutor;
 
 
     private ThreadExecutor() {
-        long keepAlive = KEEP_ALIVE_TIME;
         mThreadPoolExecutor = new ThreadPoolExecutor(
                 CORE_POOL_SIZE,
                 MAX_POOL_SIZE,
-                keepAlive,
+                KEEP_ALIVE_TIME,
                 TIME_UNIT,
                 WORK_QUEUE);
     }
